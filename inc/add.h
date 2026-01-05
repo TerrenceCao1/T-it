d7d356887244f1562672ac6756f01d7eea719466
Terrence Cao <terrencezcao@gmail.com>
2026-01-05 14:12:57 -0800

    added documentation

--- inc/add.h
+++ inc/add.h
@@ -23,13 +23,48 @@ typedef struct indexEntry
 } indexEntry;
 
 
-
+/*
+* @brief	initialize .tit/index with just a header and a checksum, no entries
+*
+* @return	0 if successful, -1 if failed
+*
+*/
 int initIndex(void);
 
+/*
+* @brief	read the index, and store the entries into the indexEntry** entries array, and entry count
+*
+* @param	entries - pointer to array of pointers of indexEntries, will be allocated and populated by readIndex function
+*
+* @param	count - pointer to size_t entry counter, will be populated by readIndex function
+*
+* @return	0 if successful, -1 if failed
+*/
 int readIndex(struct indexEntry** entries, size_t* count);
 
+/*
+* @brief	free the entries array and count 
+*
+* @param	entries - array of indexEntries that will be freed
+*
+* @param	count - entry counter to be freed
+*
+* @return	0 if successful, -1 if failed
+*
+*/
 int freeEntriesArr(struct indexEntry** entries, size_t count);
 
+/*
+* @brief	adds a file to the index/staging area
+*
+* @param	file - file name to be added
+*
+* @param	entries - array of indexEntries used to add files
+* 
+* @param	count - pointer to size_t entry counter, used under the hood
+*
+* @return	0 if successful, -1 if failed
+*/
 int addFile(char* file, struct indexEntry** entries, size_t* count);
 
 #endif
