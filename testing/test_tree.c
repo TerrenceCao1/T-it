341a358017d5da4e467f30bce3e8214bc3b4e170
Terrence Cao <terrencezcao@gmail.com>
2026-01-08 15:32:32 -0800

    new test file names

---
+++ testing/test_tree.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <linux/limits.h>
+#include "init.h"
+#include "add.h"
+#include "write-tree.h"
+
+int main(void)
+{
+	char cwd[PATH_MAX];
+	getcwd(cwd, sizeof(cwd));
+	init(cwd);
+
+	struct indexEntry* entries = NULL;
+	size_t count = 0;
+	readIndex(&entries, &count);
+
+	addFile("README.md", &entries, &count);
+	addFile("README.md", &entries, &count);
+	addFile("src/add.c", &entries, &count);
+
+	writeTree();
+
+	freeEntriesArr(&entries, count);
+
+	return 0;
+}
