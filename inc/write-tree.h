b3cf56262b99165bc3b56aa792cc1b87dfbc282a
Terrence Cao <terrencezcao@gmail.com>
2026-01-08 20:02:52 -0800

    same as above

--- inc/write-tree.h
+++ inc/write-tree.h
@@ -4,9 +4,23 @@
 #include <stdint.h>
 #include <stdio.h>
 
+/*
+* @brief	writeTree converts the current index into a tree object, hashes, and puts it into object folder
+*
+* @param	write - bool on whether to write it or not
+*
+* @return	0 if success, 1 if not
+*/
 int writeTree(_Bool write);
 
-uint8_t* hashTree(size_t treeSize);
-
+/*
+* @brief	compresses a file using zlib and puts it into the fileOut
+*
+* @param	fileIn - string with the path to the input file
+*
+* @param	fileOut - string with the path to the output file
+*
+* @return	0 if success, 1 if not
+*/
 int compressFile(char* fileIn, char* fileOut);
 #endif
